d725f5592d1b87ad4ad4d2c80c4234e36d24c6e2
Prashant <rautela49@gmail.com>
2021-06-27 00:17:47 +0530

    my feature branch changes

--- src/test/java/HomeTest.java
+++ src/test/java/HomeTest.java
@@ -5,7 +5,15 @@ public class HomeTest {
 	
 	@Test
 	public void test1() {
-		System.out.println("this is test1");
+		System.out.println("this is feature change");
 	}
 
+	
+	@Test
+	public void test2() {
+		
+		System.out.println("changes on feature branch");
+		
+	}
+	
 }
