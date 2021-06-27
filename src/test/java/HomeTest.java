50363bbe6f45f5aa3a280a7f2a40cdc4669db586
Prashant <rautela49@gmail.com>
2021-06-27 15:08:25 +0530

    featureBranchChanges

--- src/test/java/HomeTest.java
+++ src/test/java/HomeTest.java
@@ -1,22 +1,7 @@
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
 public class HomeTest {
 	
 	
-	@Test
-	public void test1() {
-		System.out.println("this is feature change");
-	}
-
 	
-	@Test
-	public void test2() {
-		
-		System.out.println("changes on feature branch");
-		
-		Assert.assertEquals(false, false);
-		
-	}
+	
 	
 }
