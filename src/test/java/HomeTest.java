a844897c9a1a695852306a2cecd396c2a326272a
Prashant <rautela49@gmail.com>
2021-07-01 09:18:56 +0530

    Revert "commiting some changes"
    This reverts commit 3d858f98af5a66a981ed1564a850584e3dc33fc1.

--- src/test/java/HomeTest.java
+++ src/test/java/HomeTest.java
@@ -1,21 +1,7 @@
-import org.testng.annotations.Test;
-
 public class HomeTest {
 	
 	
-	@Test (groups = "QA")
-	public void test1() {
-		System.out.println("this is QA group");
-		
-		
-	}
 	
-	@Test (groups = "stage")
-	public void test2() {
-		System.out.println("this is stage");
-		
-		
-	}
 	
 	
 }
