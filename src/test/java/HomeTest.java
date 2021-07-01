3d858f98af5a66a981ed1564a850584e3dc33fc1
Prashant <rautela49@gmail.com>
2021-07-01 09:16:44 +0530

    commiting some changes

--- src/test/java/HomeTest.java
+++ src/test/java/HomeTest.java
@@ -1,7 +1,21 @@
+import org.testng.annotations.Test;
+
 public class HomeTest {
 	
 	
+	@Test (groups = "QA")
+	public void test1() {
+		System.out.println("this is QA group");
+		
+		
+	}
 	
+	@Test (groups = "stage")
+	public void test2() {
+		System.out.println("this is stage");
+		
+		
+	}
 	
 	
 }
