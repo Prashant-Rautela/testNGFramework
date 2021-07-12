8cbefd87c34cd5792b1d3d1ddf0ab7e500a2ca93
Prashant <rautela49@gmail.com>
2021-07-12 22:23:07 +0530

    mylatestchanges

--- src/test/java/HomeTest.java
+++ src/test/java/HomeTest.java
@@ -1,7 +1,16 @@
+import org.testng.annotations.Test;
+
 public class HomeTest {
 	
+	@Test
+	public void testRun() {
+		System.out.println("this test needs to run");
+	}
 	
-	
+	@Test
+	public void testSkip() {
+		System.out.println("this test needs to skip");
+	}
 	
 	
 }
